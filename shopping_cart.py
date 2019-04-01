105ddfb7a74d0a0ee7a3b37c286b19cad3344688
steeznation16 <awester17@gmail.com>
2019-04-01 07:52:38 -0400

    updating all files

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,3 +1,11 @@
-class ShoppingCart:
+class ShoppingCart():
     # write your code here
-    pass
+    def __init__(self, emp_discount=None):
+        self._total = 0
+        self._employee_discount = emp_discount
+        self._items = []
+
+    def add_item(self, name, price, quantity=1):
+        self._items.append({"name": name, "price": price})
+        self._total += price
+        return self.total
